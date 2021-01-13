f11a688030a940ac1321a0c6803f4852521f4dc3
MarkLimML <mark_lim@dlsu.edu.ph>
2021-01-13 23:58:32 +0800

    Update files

--- gen/Java8ErrorListener.java
+++ gen/Java8ErrorListener.java
@@ -1,11 +1,13 @@
 import org.antlr.v4.runtime.*;
-import sun.plugin2.util.SystemUtil;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Java8ErrorListener extends BaseErrorListener {
     public int lastError = -1;
+
+    public List<String> Errors = new ArrayList<>();
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer,
                             Object offendingSymbol,
@@ -29,20 +31,27 @@ public class Java8ErrorListener extends BaseErrorListener {
         // Collections.reverse(stack);
 
 
-
+        /*
         if (thisError > lastError + 10) {
             lastError = thisError - 10;
         }
         for (int idx = lastError + 1; idx <= thisError; idx++) {
             Token token = tokens.get(idx);
-            if (token.getChannel() != Token.HIDDEN_CHANNEL) System.err.println(token.toString());
+            if (token.getChannel() != Token.HIDDEN_CHANNEL)
+                System.err.println(token.toString());
         }
         lastError = thisError;
+        */
+
 
         //List<String> stack = ((Parser)recognizer).getRuleInvocationStack();
         //Collections.reverse(stack);
         //System.err.println("rule stack: "+stack);
-        System.err.println("ERROR: line "+line+":"+charPositionInLine+" at "+
-                offendingSymbol+": "+msg);
+        //System.err.println("ERROR: at line "+line+":"+charPositionInLine+" : "+msg);
+        Errors.add("ERROR: at line "+line+":"+charPositionInLine+" : "+msg);
+    }
+
+    public List<String> getErrors() {
+        return Errors;
     }
 }
